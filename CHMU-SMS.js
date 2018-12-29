f0cbd05540b31d7effd5a16443e82dbba8b61aa8
Jan Urbánek <git@zirland.org>
2018-12-29 13:33:13 +0100

    Úprava nadpisů

--- CHMU-SMS.js
+++ CHMU-SMS.js
@@ -6,19 +6,19 @@ var detailni = 0;
 var KRAJE_NAZVY = {
     "-1": "Česká republika",
     "19": "Hlavní město Praha",
-    "27": "Středočeský",
-    "35": "Jihočeský",
-    "43": "Plzeňský",
-    "51": "Karlovarský",
-    "60": "Ústecký",
-    "78": "Liberecký",
-    "86": "Královéhradecký",
-    "94": "Pardubický",
-    "108": "Vysočina",
-    "116": "Jihomoravský",
-    "124": "Olomoucký",
-    "132": "Moravskoslezský",
-    "141": "Zlínský"
+    "27": "Středočeský kraj",
+    "35": "Jihočeský kraj",
+    "43": "Plzeňský kraj",
+    "51": "Karlovarský kraj",
+    "60": "Ústecký kraj",
+    "78": "Liberecký kraj",
+    "86": "Královéhradecký kraj",
+    "94": "Pardubický kraj",
+    "108": "Vysočina kraj",
+    "116": "Jihomoravský kraj",
+    "124": "Olomoucký kraj",
+    "132": "Moravskoslezský kraj",
+    "141": "Zlínský kraj"
 };
  
 var zacatky = [];
