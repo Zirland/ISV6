8a0eed6cafede1f6bb098ad30d161db32c177e33
Jan Urbánek <git@zirland.org>
2019-01-08 00:34:19 +0100

    Výpis krajů pro stupeň jevu

--- CHMU-IVR.js
+++ CHMU-IVR.js
@@ -42,6 +42,16 @@ var KRAJE_KODY  = {
     "141": "ZLK"
 };
 
+function removeDuplicates(arr) {
+    var unique_array = []
+    for(var i = 0;i < arr.length; i++){
+        if(unique_array.indexOf(arr[i]) == -1){
+            unique_array.push(arr[i])
+        }
+    }
+    return unique_array
+}
+
 var resultText = vystupText = '';
 
 if (vystraha.info)
@@ -49,167 +59,191 @@ if (vystraha.info)
     var poleJevy = [];
     var posledniJev = "";
     for (var i = 0; i < vystraha.info.length; i++) {
-        if (posledniJev != vystraha.info[i].stupen_kod) {
+        if (posledniJev != vystraha.info[i].stupen_kod && vystraha.info[i].stupen_kod != "OUTLOOK") {
             poleJevy.push(vystraha.info[i].stupen_kod);
         }
     }
 
-    for (var i = 0; i < poleJevy.length; i++) {
-        resultText += poleJevy[i] + '\n';
+    poleJevy = removeDuplicates(poleJevy);
+
+    for (var h = 0; h < poleJevy.length; h++) {
+        var jevKrajeList = [];
+        for (var i = 0; i < vystraha.info.length; i++) {
+            if (poleJevy[h] == vystraha.info[i].stupen_kod) {
+                var found = omezitNaKraj == -1;
+                for (var j = 0; j < vystraha.info[i].kraj.length && !found; j++) {
+                    found = vystraha.info[i].kraj[j].UID == omezitNaKraj;
+                }
+                for (var j = 0; j < vystraha.info[i].kraj.length; j++) {
+                    if (found) {
+                        jevKrajeList.push(vystraha.info[i].kraj[j].UID);
+                    }
+                }
+            }
+        }
+        jevKrajeList = removeDuplicates(jevKrajeList);
+        jevKrajeList = jevKrajeList.sort(function (a, b) {return a-b});
+
+        if (jevKrajeList.length > 0) {
+            resultText += poleJevy[h] + '\n';
+        }
+        for (var l = 0; l < jevKrajeList.length; l++) {
+            resultText += jevKrajeList[l] + '\n';
+        }
     }
 
-/*    for (var i = 0; i < vystraha.info.length; i++)
-    {
-        var found = omezitNaKraj == -1;
-        for (var j = 0; j < vystraha.info[i].kraj.length && !found; j++)
-        {
-            found = vystraha.info[i].kraj[j].UID == omezitNaKraj;
-        }
-
-        if (found)
-        {
-            zacatek = vystraha.info[i].dc_zacatek.toString();
-            zacatekDen = zacatek.substring(0,2);
-            zacatekDen_porovn = zacatekDen.replace(/\.$/, "");
-            if (zacatekDen == zacatekDen_porovn) {
-                zacatekMesic = zacatek.substring(3,5);
-                zacatekMesic_porovn = zacatekMesic.replace(/\.$/, "");
-                if (zacatekMesic == zacatekMesic_porovn) {
-                    zacatekRok = zacatek.substring(6,10)
-                    zacatekCas = zacatek.substring(11,16);
-                    zacatekCas = zacatekCas.replace(/\:$/, "");
-                } else {
-                    zacatekMesic = '0' + zacatekMesic_porovn;
-                    zacatekRok = zacatek.substring(5,9)
-                    zacatekCas = zacatek.substring(10,15);
-                    zacatekCas = zacatekCas.replace(/\:$/, "");
-                }
-            } else {
-                zacatekDen = '0' + zacatekDen_porovn;
-                zacatekMesic = zacatek.substring(2,4);
-                zacatekMesic_porovn = zacatekMesic.replace(/\.$/, "");
-                if (zacatekMesic == zacatekMesic_porovn) {
-                    zacatekRok = zacatek.substring(5,9)
-                    zacatekCas = zacatek.substring(10,15);
-                    zacatekCas = zacatekCas.replace(/\:$/, "");
-                } else {
-                    zacatekMesic = '0' + zacatekMesic_porovn;
-                    zacatekRok = zacatek.substring(4,8)
-                    zacatekCas = zacatek.substring(9,14);
-                    zacatekCas = zacatekCas.replace(/\:$/, "");
-                }
+/*    for (var h = 0; h < poleJevy.length; h++) {
+        for (var i = 0; i < vystraha.info.length; i++) {
+            var found = omezitNaKraj == -1;
+            for (var j = 0; j < vystraha.info[i].kraj.length && !found; j++)
+            {
+                found = vystraha.info[i].kraj[j].UID == omezitNaKraj;
             }
 
-            zacatekHodina = zacatekCas.substring(0,2);
-            zacatekHodina_porovn = zacatekHodina.replace(/\:$/, "");
-            if (zacatekHodina == zacatekHodina_porovn) {
-                zacatekMinuta = zacatekCas.substring(3,5);
-            } else {
-                zacatekHodina = '0' + zacatekHodina_porovn;
-                zacatekMinuta = zacatekCas.substring(2,4);
-            }
-
-            zacatek_format = zacatekRok + zacatekMesic + zacatekDen + zacatekHodina + zacatekMinuta;
-            zacatek_format_num = Number(zacatek_format);
-
-            if (vystraha.info[i].jev_kod != "OUTLOOK") {
-                zacatky.push(zacatek_format_num);
-            }
-
-            konec = '999999999999';
-            if (vystraha.info[i].dc_konec) {
-                konec = vystraha.info[i].dc_konec.toString();
-            }
-            
-            konecDen = konec.substring(0,2);
-            konecDen_porovn = konecDen.replace(/\.$/, "");
-            if (konecDen == konecDen_porovn) {
-                konecMesic = konec.substring(3,5);
-                konecMesic_porovn = konecMesic.replace(/\.$/, "");
-                if (konecMesic == konecMesic_porovn) {
-                    konecRok = konec.substring(6,10)
-                    konecCas = konec.substring(11,16);
-                    konecCas = konecCas.replace(/\:$/, "");
-                } else {
-                    konecMesic = '0' + konecMesic_porovn;
-                    konecRok = konec.substring(5,9)
-                    konecCas = konec.substring(10,15);
-                    konecCas = konecCas.replace(/\:$/, "");
-                }
-            } else {
-                konecDen = '0' + konecDen_porovn;
-                konecMesic = konec.substring(2,4);
-                konecMesic_porovn = konecMesic.replace(/\.$/, "");
-                if (konecMesic == konecMesic_porovn) {
-                    konecRok = konec.substring(5,9)
-                    konecCas = konec.substring(10,15);
-                    konecCas = konecCas.replace(/\:$/, "");
-                } else {
-                    konecMesic = '0' + konecMesic_porovn;
-                    konecRok = konec.substring(4,8)
-                    konecCas = konec.substring(9,14);
-                    konecCas = konecCas.replace(/\:$/, "");
-                }
-            }
-
-            konecHodina = konecCas.substring(0,2);
-            konecHodina_porovn = konecHodina.replace(/\:$/, "");
-            if (konecHodina == konecHodina_porovn) {
-                konecMinuta = konecCas.substring(3,5);
-            } else {
-                konecHodina = '0' + konecHodina_porovn;
-                konecMinuta = konecCas.substring(2,4);
-            }
-
-            konec_format = konecRok + konecMesic + konecDen + konecHodina + konecMinuta;
-            konec_format_num = Number(konec_format);
-            if (konec == "999999999999") {
-                konec_format_num = 999999999999;
-            }
-
-            zahajeni = zacatek_format.substring(6,8) + '.' + zacatek_format.substring(4,6) + '. ' + zacatek_format.substring(8,10) + ':' + zacatek_format.substring(10,12);
-            ukonceni = konec_format.substring(6,8) + '.' + konec_format.substring(4,6) + '. ' + konec_format.substring(8,10) + ':' + konec_format.substring(10,12);
-            if (konec == "999999999999") {
-                ukonceni = 'odvolání.';
-            }
-
-            if (vystraha.info[i].jev_kod != "OUTLOOK") {
-                konce.push(konec_format_num);
-                warn_type = "SVRS";
-                if (vystraha.info[i].SIVS == "1") {
-                    warn_type = "SIVS";
-                }
-                if (vystraha.info[i].HPPS == "1") {
-                    warn_type = "HPPS";
-                }
-                seznjevu.push(warn_type);
-
-                if (omezitNaKraj == -1) {
-                    resultText += vystraha.info[i].jev;
-                    resultText += ' pro kraje ';
-
-                    var seznkraje = '';
-                    var kraje_pole = [];
-
-                    for (var t = 0; t < vystraha.info[i].kraj.length; t++) {
-                        kraje_pole.push(vystraha.info[i].kraj[t].UID);
-                    }
-
-                    kraje_pole.sort(function(a, b){return a - b});
-
-                    for (var t = 0; t < vystraha.info[i].kraj.length; t++) {
-                        seznkraje += '' + KRAJE_KODY[kraje_pole[t]] + ', ';
-                    }
-                    seznkraje = seznkraje.substring(0, seznkraje.length-2);
-                    resultText += seznkraje + '\n';
-                } else {
-                    if (detailni) {
-                        resultText += vystraha.info[i].jev + ' od ' + zahajeni + ' do ' + ukonceni + '\n';
+            if (found)
+            {
+                zacatek = vystraha.info[i].dc_zacatek.toString();
+                zacatekDen = zacatek.substring(0,2);
+                zacatekDen_porovn = zacatekDen.replace(/\.$/, "");
+                if (zacatekDen == zacatekDen_porovn) {
+                    zacatekMesic = zacatek.substring(3,5);
+                    zacatekMesic_porovn = zacatekMesic.replace(/\.$/, "");
+                    if (zacatekMesic == zacatekMesic_porovn) {
+                        zacatekRok = zacatek.substring(6,10)
+                        zacatekCas = zacatek.substring(11,16);
+                        zacatekCas = zacatekCas.replace(/\:$/, "");
                     } else {
-                        resultText += vystraha.info[i].jev + '\n';
+                        zacatekMesic = '0' + zacatekMesic_porovn;
+                        zacatekRok = zacatek.substring(5,9)
+                        zacatekCas = zacatek.substring(10,15);
+                        zacatekCas = zacatekCas.replace(/\:$/, "");
+                    }
+                } else {
+                    zacatekDen = '0' + zacatekDen_porovn;
+                    zacatekMesic = zacatek.substring(2,4);
+                    zacatekMesic_porovn = zacatekMesic.replace(/\.$/, "");
+                    if (zacatekMesic == zacatekMesic_porovn) {
+                        zacatekRok = zacatek.substring(5,9)
+                        zacatekCas = zacatek.substring(10,15);
+                        zacatekCas = zacatekCas.replace(/\:$/, "");
+                    } else {
+                        zacatekMesic = '0' + zacatekMesic_porovn;
+                        zacatekRok = zacatek.substring(4,8)
+                        zacatekCas = zacatek.substring(9,14);
+                        zacatekCas = zacatekCas.replace(/\:$/, "");
                     }
                 }
-            }
+
+                zacatekHodina = zacatekCas.substring(0,2);
+                zacatekHodina_porovn = zacatekHodina.replace(/\:$/, "");
+                if (zacatekHodina == zacatekHodina_porovn) {
+                    zacatekMinuta = zacatekCas.substring(3,5);
+                } else {
+                    zacatekHodina = '0' + zacatekHodina_porovn;
+                    zacatekMinuta = zacatekCas.substring(2,4);
+                }
+
+                zacatek_format = zacatekRok + zacatekMesic + zacatekDen + zacatekHodina + zacatekMinuta;
+                zacatek_format_num = Number(zacatek_format);
+
+                if (vystraha.info[i].jev_kod != "OUTLOOK") {
+                    zacatky.push(zacatek_format_num);
+                }
+
+                konec = '999999999999';
+                if (vystraha.info[i].dc_konec) {
+                    konec = vystraha.info[i].dc_konec.toString();
+                }
+                
+                konecDen = konec.substring(0,2);
+                konecDen_porovn = konecDen.replace(/\.$/, "");
+                if (konecDen == konecDen_porovn) {
+                    konecMesic = konec.substring(3,5);
+                    konecMesic_porovn = konecMesic.replace(/\.$/, "");
+                    if (konecMesic == konecMesic_porovn) {
+                        konecRok = konec.substring(6,10)
+                        konecCas = konec.substring(11,16);
+                        konecCas = konecCas.replace(/\:$/, "");
+                    } else {
+                        konecMesic = '0' + konecMesic_porovn;
+                        konecRok = konec.substring(5,9)
+                        konecCas = konec.substring(10,15);
+                        konecCas = konecCas.replace(/\:$/, "");
+                    }
+                } else {
+                    konecDen = '0' + konecDen_porovn;
+                    konecMesic = konec.substring(2,4);
+                    konecMesic_porovn = konecMesic.replace(/\.$/, "");
+                    if (konecMesic == konecMesic_porovn) {
+                        konecRok = konec.substring(5,9)
+                        konecCas = konec.substring(10,15);
+                        konecCas = konecCas.replace(/\:$/, "");
+                    } else {
+                        konecMesic = '0' + konecMesic_porovn;
+                        konecRok = konec.substring(4,8)
+                        konecCas = konec.substring(9,14);
+                        konecCas = konecCas.replace(/\:$/, "");
+                    }
+                }
+
+                konecHodina = konecCas.substring(0,2);
+                konecHodina_porovn = konecHodina.replace(/\:$/, "");
+                if (konecHodina == konecHodina_porovn) {
+                    konecMinuta = konecCas.substring(3,5);
+                } else {
+                    konecHodina = '0' + konecHodina_porovn;
+                    konecMinuta = konecCas.substring(2,4);
+                }
+
+                konec_format = konecRok + konecMesic + konecDen + konecHodina + konecMinuta;
+                konec_format_num = Number(konec_format);
+                if (konec == "999999999999") {
+                    konec_format_num = 999999999999;
+                }
+
+                zahajeni = zacatek_format.substring(6,8) + '.' + zacatek_format.substring(4,6) + '. ' + zacatek_format.substring(8,10) + ':' + zacatek_format.substring(10,12);
+                ukonceni = konec_format.substring(6,8) + '.' + konec_format.substring(4,6) + '. ' + konec_format.substring(8,10) + ':' + konec_format.substring(10,12);
+                if (konec == "999999999999") {
+                    ukonceni = 'odvolání.';
+                }
+
+                if (vystraha.info[i].jev_kod != "OUTLOOK") {
+                    konce.push(konec_format_num);
+                    warn_type = "SVRS";
+                    if (vystraha.info[i].SIVS == "1") {
+                        warn_type = "SIVS";
+                    }
+                    if (vystraha.info[i].HPPS == "1") {
+                        warn_type = "HPPS";
+                    }
+                    seznjevu.push(warn_type);
+
+                    if (omezitNaKraj == -1) {
+                        resultText += vystraha.info[i].jev;
+                        resultText += ' pro kraje ';
+
+                        var seznkraje = '';
+                        var kraje_pole = [];
+
+                        for (var t = 0; t < vystraha.info[i].kraj.length; t++) {
+                            kraje_pole.push(vystraha.info[i].kraj[t].UID);
+                        }
+
+                        kraje_pole.sort(function(a, b){return a - b});
+
+                        for (var t = 0; t < vystraha.info[i].kraj.length; t++) {
+                            seznkraje += '' + KRAJE_KODY[kraje_pole[t]] + ', ';
+                        }
+                        seznkraje = seznkraje.substring(0, seznkraje.length-2);
+                        resultText += seznkraje + '\n';
+                    } else {
+                        if (detailni) {
+                            resultText += vystraha.info[i].jev + ' od ' + zahajeni + ' do ' + ukonceni + '\n';
+                        } else {
+                            resultText += vystraha.info[i].jev + '\n';
+                        }
+                    }
+                }
         }
     }
 
