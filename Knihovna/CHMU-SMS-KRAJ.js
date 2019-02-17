4d55faf1db563e731b8b49c628fa49a38a91c4b6
Jan Urbánek <git@zirland.org>
2019-02-17 10:08:32 +0100

    Ciselnik, diffs

--- Knihovna/CHMU-SMS-KRAJ.js
+++ Knihovna/CHMU-SMS-KRAJ.js
@@ -1,69 +1,10 @@
 // Verze 26
 
+#import "CHMU-CISELNIK";
+
 var zacatky = [];
 var konce = [];
 var seznjevu = [];
-
-var KRAJE_KODY  = { "19": "PHA", "27": "SČK", "35": "JČK", "43": "PLK", "51": "KVK", "60": "ULK", "78": "LIK", "86": "KHK", "94": "PAK", "108": "VYK", "116": "JMK", "124": "OLK", "132": "MSK", "141": "ZLK" };
-
-var JEVY_NAZVY = {
-    "I.1" : "Vysoké teploty",
-    "I.2" : "Velmi vysoké teploty",
-    "I.3" : "Extrémně vysoké teploty",
-    "I.4" : "Silný mráz",
-    "I.5" : "Velmi silný mráz",
-    "I.6" : "Extrémní mráz",
-    "II.1" : "Mráz ve vegetačním období",
-    "II.2" : "Prudký pokles teploty",
-    "III.1" : "Silný vítr",
-    "III.2" : "Velmi silný vítr",
-    "III.3" : "Extrémně silný vítr",
-    "IV.1" : "Nová sněhová pokrývka",
-    "IV.2" : "Vysoká nová sněhová pokrývka",
-    "IV.3" : "Extrémní sněhová pokrývka",
-    "IV.4" : "Vysoká celková sněhová pokrývka",
-    "V.1" : "Silné sněžení",
-    "V.2" : "Extrémně silné sněžení",
-    "VI.1" : "Sněhové jazyky",
-    "VI.2" : "Závěje",
-    "VI.3" : "Sněhová bouře",
-    "VII.1" : "Náledí",
-    "VIII.1" : "Ledovka",
-    "VIII.2" : "Silná ledovka",
-    "VIII.3" : "Velmi silná ledovka",
-    "IX.1" : "Mrznoucí mlhy",
-    "IX.2" : "Silná námraza ",
-    "X.1" : "Silné bouřky",
-    "X.2" : "Velmi silné bouřky",
-    "X.2a" : "Velmi silné bouřky s přívalovými srážkami",
-    "X.3" : "Extrémně silné bouřky",
-    "X.3a" : "Extrémně silné bouřky s přívalovými srážkami",
-    "XI.1" : "Vydatný déšť",
-    "XI.2" : "Velmi vydatný déšť",
-    "XI.3" : "Extrémní srážky",
-    "XII.1" : "Povodňová bdělost",
-    "XII.2" : "Povodňová pohotovost",
-    "XII.3" : "Povodňové ohrožení",
-    "XII.4" : "Extrémní povodňové ohrožení",
-    "XIII.1" : "Povodňová bdělost – dotok",
-    "XIII.2" : "Povodňová pohotovost – dotok",
-    "XIII.3" : "Povodňové ohrožení – dotok",
-    "XIII.4" : "Extrémní povodňové ohrožení – dotok",
-    "XIV.1" : "Nebezpečí požárů",
-    "XIV.2" : "Vysoké nebezpečí požárů",
-    "XV.1" : "Jiný jev",
-    "XV.2" : "Jiný jev",
-    "XV.3" : "Jiný jev",
-    "OUTLOOK" : "Výhled nebezpečných jevů",
-    "SMOGSIT.O3" : "Smogová situace O3",
-    "WARN.O3" : "Varování O3",
-    "SMOGSIT.PM10" : "Smogová situace PM10",
-    "REG.PM10" : "Regulace PM10",
-    "SMOGSIT.SO2" : "Smogová situace SO2",
-    "REG.SO2" : "Regulace SO2",
-    "SMOGSIT.NO2" : "Smogová situace NO2",
-    "REG.NO2" : "Regulace NO2",
-};
 
 // Odstranění duplicitních výskytů kódů jevů
 function removeDuplicates(arr) {
