a8f4d1cc98175a28766ba0f356a3ced55220bc55
Jan Urbánek <git@zirland.org>
2019-02-17 11:27:25 +0100

    Datumy

---
+++ Knihovna/CHMU-DATUMY.js
@@ -0,0 +1,113 @@
+//Verze 26
+
+// Úprava formátu data
+function Normalize(datum) {
+    var datumString = datum.toString();
+
+    datumDen = datumString.substring(8,10);
+    datumMesic = datumString.substring(5,7);
+    datumRok = datumString.substring(0,4)
+    datumCas = datumString.substring(11,19);
+    datumHodiny = datumCas.substring(0,2);
+    datumMinuty = datumCas.substring(3,5);
+    datumSekundy = datumCas.substring(6,8)
+
+    datum = datumRok + datumMesic + datumDen + datumHodiny + datumMinuty + datumSekundy;
+
+    return datum;
+}
+
+// Zjišťuje, zda je konecJev nenastane v období 30 minut od casZpravy
+function UkoncenyJev(konecJev, casZprava) {
+    if (!konecJev) {
+        konecJev = '1.1.2100 01:00:00';
+    }
+
+    var konecJev_format = Normalize(konecJev);
+    var casZprava_format = Normalize(casZprava);
+
+    var kjYear = konecJev_format.substring(0,4);
+    var kjMonth = konecJev_format.substring(4,6);
+    var kjDay = konecJev_format.substring(6,8);
+    var kjHour = konecJev_format.substring(8,10);
+    var kjMinute = konecJev_format.substring(10,12);
+    var myEndTime = new Date(kjYear, kjMonth-1, kjDay, kjHour, kjMinute, '00');
+
+    myEndTime.setMinutes(myEndTime.getMinutes() - 30);
+    konecJev_format = Normalize(myEndTime);
+
+    konecJev_format_num = Number(konecJev_format);
+    casZprava_format_num = Number(casZprava_format);
+
+    output = konecJev_format_num + '<' + casZprava_format_num;
+
+    if (konecJev_format_num < casZprava_format_num) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ZobrazDatum(datum, format, end) {
+    if (!datum) {
+        format_datum = 'do odvolání';
+    } else {
+        var normDatum = Normalize(datum);
+
+        var normDatumRok = normDatum.substring(0,4);
+        var normDatumMesic = normDatum.substring(4,6);
+        var normDatumDen = normDatum.substring(6,8);
+        var normDatumHodina = normDatum.substring(8,10);
+        var normDatumMinuta = normDatum.substring(10,12);
+        var normDatumSekunda = '00';
+
+        if (normDatumHodina == '00' && normDatumMinuta == '00' && end) {
+            var myNewDay = new Date(normDatumRok, normDatumMesic-1, normDatumDen-1);
+            var newNormDatum = Normalize(myNewDay);
+            normDatumRok = newNormDatum.substring(0,4);
+            normDatumMesic = newNormDatum.substring(4,6);
+            normDatumDen = newNormDatum.substring(6,8);
+            normDatumHodina = '24';
+        }
+
+        switch (format) {
+            case 'short' :
+                format_datum = Number(normDatumDen) + '.' + Number(normDatumMesic) + '. ' + normDatumHodina + ':' + normDatumMinuta;
+            break;
+            case 'long' :
+                format_datum = Number(normDatumDen) + '.' + Number(normDatumMesic) + '.' + normDatumRok + ' ' + normDatumHodina + ':' + normDatumMinuta + ':' + normDatumSekunda;
+        }
+        
+    }
+
+    return format_datum;
+}
+
+function ZobrazDatumSMS(datum, end) {
+    if (datum == 99999999999999) {
+        format_datum = 'odvolání';
+    } else {
+        var normDatum = datum.toString();
+
+        var normDatumRok = normDatum.substring(0,4);
+        var normDatumMesic = normDatum.substring(4,6);
+        var normDatumDen = normDatum.substring(6,8);
+        var normDatumHodina = normDatum.substring(8,10);
+        var normDatumMinuta = normDatum.substring(10,12);
+        var normDatumSekunda = '00'; 
+
+        if (normDatumHodina == '00' && normDatumMinuta == '00' && end) {
+            var myNewDay = new Date(normDatumRok, normDatumMesic-1, normDatumDen-1);
+            var newNormDatum = Normalize(myNewDay);
+            normDatumRok = newNormDatum.substring(0,4);
+            normDatumMesic = newNormDatum.substring(4,6);
+            normDatumDen = newNormDatum.substring(6,8);
+            normDatumHodina = '24';
+        }
+
+        format_datum = Number(normDatumDen) + '.' + Number(normDatumMesic) + '. ' + normDatumHodina + ':' + normDatumMinuta;
+    }
+
+    return format_datum;
+}
+
--- Knihovna/CHMU-SMS-KRAJ.js
+++ Knihovna/CHMU-SMS-KRAJ.js
@@ -1,6 +1,7 @@
 // Verze 26
 
 #import "CHMU-CISELNIK";
+#import "CHMU-DATUMY";
 
 var zacatky = [];
 var konce = [];
@@ -15,79 +16,6 @@ function removeDuplicates(arr) {
         }
     }
     return unique_array
-}
-
-// Úprava formátu data
-function Normalize(datum) {
-    var datumString = datum.toString();
-
-    datumDen = datumString.substring(8,10);
-    datumMesic = datumString.substring(5,7);
-    datumRok = datumString.substring(0,4)
-    datumCas = datumString.substring(11,16);
-    datumHodiny = datumCas.substring(0,2);
-    datumMinuty = datumCas.substring(3,5);
-
-    datum = datumRok + datumMesic + datumDen + datumHodiny + datumMinuty;
-
-    return datum;
-}
-
-function UkoncenyJev(konecJev, casZprava) {
-    if (!konecJev) {
-        konecJev = '1.1.2100 01:00';
-    }
-
-    var konecJev_format = Normalize(konecJev);
-    var casZprava_format = Normalize(casZprava);
-
-    var kjYear = konecJev_format.substring(0,4);
-    var kjMonth = konecJev_format.substring(4,6);
-    var kjDay = konecJev_format.substring(6,8);
-    var kjHour = konecJev_format.substring(8,10);
-    var kjMinute = konecJev_format.substring(10,12);
-    var myEndTime = new Date(kjYear, kjMonth-1, kjDay, kjHour, kjMinute);
-
-    myEndTime.setMinutes(myEndTime.getMinutes() - 30);
-    konecJev_format = Normalize(myEndTime);
-
-    konecJev_format_num = Number(konecJev_format);
-    casZprava_format_num = Number(casZprava_format);
-
-    output = konecJev_format_num + '<' + casZprava_format_num;
-
-    if (konecJev_format_num < casZprava_format_num) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function ZobrazDatum(datum, end) {
-    if (datum == 999999999999) {
-        format_datum = 'odvolání';
-    } else {
-        var normDatum = datum.toString();
-
-        var normDatumRok = normDatum.substring(0,4);
-        var normDatumMesic = normDatum.substring(4,6);
-        var normDatumDen = normDatum.substring(6,8);
-        var normDatumHodina = normDatum.substring(8,10);
-        var normDatumMinuta = normDatum.substring(10,12);
-
-        if (normDatumHodina == '00' && normDatumMinuta == '00' && end) {
-            var myNewDay = new Date(normDatumRok, normDatumMesic-1, normDatumDen-1);
-            var newNormDatum = Normalize(myNewDay);
-            normDatumRok = newNormDatum.substring(0,4);
-            normDatumMesic = newNormDatum.substring(4,6);
-            normDatumDen = newNormDatum.substring(6,8);
-            normDatumHodina = '24';
-        }
-
-        format_datum = Number(normDatumDen) + '.' + Number(normDatumMesic) + '. ' + normDatumHodina + ':' + normDatumMinuta;
-    }
-
-    return format_datum;
 }
 
 var resultText = vystupText = '';
@@ -130,14 +58,14 @@ if (vystraha.info)
                         seznjevu.push(warn_type);
                         zacatek = Normalize(vystraha.info[i].dc_zacatek);
                         zacatky.push(zacatek);
-                        konec = 999999999999;
+                        konec = 99999999999999;
                         if (vystraha.info[i].dc_konec) {
                             konec = Normalize(vystraha.info[i].dc_konec);
                         }
                         konce.push(konec);
 
-                        zahajeni = ZobrazDatum(zacatek);
-                        ukonceni = ZobrazDatum(konec, 1);
+                        zahajeni = ZobrazDatumSMS(zacatek);
+                        ukonceni = ZobrazDatumSMS(konec, 1);
                     }
                 }
             }
@@ -179,8 +107,8 @@ if (vystraha.info)
     endy = Math.max.apply(null, konce);
     end = endy.toString();
 
-    total_zahajeni = ZobrazDatum(start);
-    total_ukonceni = ZobrazDatum(end, 1);
+    total_zahajeni = ZobrazDatumSMS(start);
+    total_ukonceni = ZobrazDatumSMS(end, 1);
 
     // Sestavíme hlavičku zprávy
     rezim = 'SVRS';
@@ -267,14 +195,14 @@ if (typeof(ref_vystraha) != 'undefined' && ref_vystraha.info && ref_vystraha.inf
 
                         zacatek = Normalize(ref_vystraha.info[i].dc_zacatek);
                         zacatky.push(zacatek);
-                        konec = 999999999999;
+                        konec = 99999999999999;
                         if (ref_vystraha.info[i].dc_konec) {
                             konec = Normalize(ref_vystraha.info[i].dc_konec);
                         }
                         konce.push(konec);
 
-                        zahajeni = ZobrazDatum(zacatek);
-                        ukonceni = ZobrazDatum(konec, 1);
+                        zahajeni = ZobrazDatumSMS(zacatek);
+                        ukonceni = ZobrazDatumSMS(konec, 1);
                     }
                 }
             }
@@ -315,8 +243,8 @@ if (typeof(ref_vystraha) != 'undefined' && ref_vystraha.info && ref_vystraha.inf
     endy = Math.max.apply(null, konce);
     end = endy.toString();
 
-    total_zahajeni = ZobrazDatum(start);
-    total_ukonceni = ZobrazDatum(end, 1);
+    total_zahajeni = ZobrazDatumSMS(start);
+    total_ukonceni = ZobrazDatumSMS(end, 1);
 
     if (start != 'Infinity') {
         // Doplníme o celkovou platnost (celostátní a souhrnná sestava) a na GŘ také odkaz na OPIN WOCZ59
