9bf55f5b26f7f94ba6e85f4c6c13899ac632a150
Jan Urbánek <git@zirland.org>
2019-05-29 10:16:17 +0200

    Rušené výstrahy

--- Knihovna/CHMU-PREPARE.js
+++ Knihovna/CHMU-PREPARE.js
@@ -424,7 +424,7 @@ function PrintInfoList(krajList, ref_krajList, headers) {
                 if (first) {
                     first = false;
                     (headers === 0 ? '' : resultText += '</div><br/><div><b>' + KRAJE_NAZVY[ref_krajList[k].id] + '</b>');
-                    empty = false;
+//                    empty = false;
                 }
 
                 ref_zpracovanyInfoStupen.push(ref_info.jev_kod + (ref_info.vyska ? ref_info.vyska : '[]') + ref_info.dc_zacatek + '-' + ref_info.dc_konec);
@@ -447,7 +447,7 @@ function PrintInfoList(krajList, ref_krajList, headers) {
                         if (first) {
                             first = false;
                             (headers === 0 ? '' : resultText += '</div><br/><div><b>Okres ' + ref_krajList[k].okresList[o].nazev + '</b>');
-                            empty = false;
+//                            empty = false;
                         }
 
                         ref_zpracovanyInfoStupenOkres.push(ref_info.jev_kod + (ref_info.vyska ? ref_info.vyska : '[]') + ref_info.dc_zacatek + '-' + ref_info.dc_konec);
@@ -470,7 +470,7 @@ function PrintInfoList(krajList, ref_krajList, headers) {
                             if (first) {
                                 first = false;
                                 (headers === 0 ? '' : resultText += '</div><br/><div><b>ORP ' + ref_krajList[k].okresList[o].orpList[ol].nazev + '</b>');
-                                empty = false;
+//                                empty = false;
                             }
 
                             pomoc = PrintInfo(null, ref_info);
