aaac51498584d6cc10314f88e9e152f18421a131
Jan Urbánek <git@zirland.org>
2019-07-02 09:14:02 +0200

    sms_test

---
+++ UVG/sms_test.js
@@ -0,0 +1,24 @@
+// Verze 54
+
+var omezitNaKraj = -1;
+var detailni = 1;
+var oddelovac = '\n';
+
+
+
+var zacatky = [];
+var konce = [];
+var sms1 = '';
+var seznjevu = [];
+var resultText = '';
+var vystupText = '';
+
+
+var zacatky2 = [];
+var konce2 = [];
+var sms2 = '';
+
+
+vystupText += '\n\n1: ' + sms1 + '\n2: ' + sms2; 
+
+return vystupText;
