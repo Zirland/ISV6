bbbc9c9bf7c06a295a3780ef5949516a8e112bd3
Jan Urbánek <git@zirland.org>
2019-07-23 10:17:05 +0200

    init

---
+++ AA/AA-CHMU-EMAIL-OKRES.js
@@ -0,0 +1,39 @@
+//!JS
+// Verze 58
+
+var mojeUzemi = [
+    '779',
+    '787',
+    '795',
+];
+var nazevUzemi = "okres Jablonec nad Nisou";
+
+var razeniPodleNazvu = false;
+var zobrazitVyhled = false;
+var zobrazitZmeny = true;
+var pouzeZmeny = true;
+var distrSeznamNahore = false;
+
+var KRAJE_NAZVY = {
+    "-1": "Česká republika",
+    "19": "Hlavní město Praha",
+    "27": "Středočeský kraj",
+    "35": "Jihočeský kraj",
+    "43": "Plzeňský kraj",
+    "51": "Karlovarský kraj",
+    "60": "Ústecký kraj",
+    "78": "Liberecký kraj",
+    "86": "Královéhradecký kraj",
+    "94": "Pardubický kraj",
+    "108": "Kraj Vysočina",
+    "116": "Jihomoravský kraj",
+    "124": "Olomoucký kraj",
+    "132": "Moravskoslezský kraj",
+    "141": "Zlínský kraj"
+};
+
+#import "CHMU-EMAIL-KRAJ"; 
+
+if (resultText !== 'undefined' && resultText) {
+    print(resultText);
+}
