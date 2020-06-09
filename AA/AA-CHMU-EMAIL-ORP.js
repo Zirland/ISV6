c95ee7a07cac9e37c909d1fe7d99af9e9d5c8e08
Jan Urbánek <git@zirland.org>
2020-06-09 20:23:19 +0200

    filtr

--- AA/AA-CHMU-EMAIL-ORP.js
+++ AA/AA-CHMU-EMAIL-ORP.js
@@ -6,6 +6,24 @@ var zobrazitVyhled = false;
 var zobrazitZmeny = true;
 var pouzeZmeny = true;
 
+var KRAJE_NAZVY = {
+    '-1': 'Česká republika',
+    '19': 'Hlavní město Praha',
+    '27': 'Středočeský kraj',
+    '35': 'Jihočeský kraj',
+    '43': 'Plzeňský kraj',
+    '51': 'Karlovarský kraj',
+    '60': 'Ústecký kraj',
+    '78': 'Liberecký kraj',
+    '86': 'Královéhradecký kraj',
+    '94': 'Pardubický kraj',
+    '108': 'Kraj Vysočina',
+    '116': 'Jihomoravský kraj',
+    '124': 'Olomoucký kraj',
+    '132': 'Moravskoslezský kraj',
+    '141': 'Zlínský kraj'
+};
+
 #import "CHMU-EMAIL-ORP";
 
 if (resultText !== 'undefined' && resultText) {
